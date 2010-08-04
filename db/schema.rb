316ccaa59e02534b199d37209faa975db20fd02f
kubus-ruby <kubus.ruby@gmail.com>
2010-08-04 18:43:19 +0300

    schema.rb was auto fixed

--- db/schema.rb
+++ db/schema.rb
@@ -9,18 +9,28 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100728223345) do
+ActiveRecord::Schema.define(:version => 20100804125440) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "price",              :default => 0.0
+    t.integer  "price",              :limit => 10, :precision => 10, :scale => 0, :default => 0
     t.string   "photo_file_name"
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
 end
