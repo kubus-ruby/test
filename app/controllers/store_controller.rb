47bc4250b48e0c26562c92290bd36988ce25c3cd
kubus-ruby <kubus.ruby@gmail.com>
2010-08-04 18:40:07 +0300

    added ignore for public/assets

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,9 +2,27 @@ class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
     @time = Time.now.strftime("%Y-%m-%d %H:%M:%S")
-    
   end
 
-  
+ def add_to_cart
+   product = Product.find(params[:id])
+   @cart = find_cart
+   @cart.add_product(product)
+ end
+
+	def empty_cart
+		session[:cart] = nil
+		flash[:notice] = "Your cart is currently empty"
+		redirect_to :action => 'index'
+	end
+
+
+
+ private
+ def find_cart
+    session[:cart] ||= Cart.new
+ end
+ 
+
 
 end
