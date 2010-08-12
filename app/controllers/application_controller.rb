10dc4bcba905247a9fcb82fc7b92fb2ef0ea27ce
kubus-ruby <kubus.ruby@gmail.com>
2010-08-12 11:51:47 +0300

    before filter, preventing customers from accessing admin page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,17 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+	before_filter :authorize, :except => :login
+
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+	protected
+	def authorize
+		unless User.find_by_id(session[:user_id])
+			flash[:notice] = "Please log in"
+			redirect_to :controller => 'admin', :action => 'login'
+		end
+	end
 end
