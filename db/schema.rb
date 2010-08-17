626ddd1746de5220286fbc59b057013c64fe72e3
kubus-ruby <kubus.ruby@gmail.com>
2010-08-17 13:59:53 +0300

    CoOMMENTS: schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100812071145) do
+ActiveRecord::Schema.define(:version => 20100817090711) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "product_id"
+    t.string   "name"
+    t.text     "body"
+    t.integer  "parent_id"
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "line_items", :force => true do |t|
     t.integer  "product_id",                                :null => false
