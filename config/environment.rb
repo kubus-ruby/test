6f8c4f80668813dc440b18dd95dd79f5215ec15d
kubus-ruby <kubus.ruby@gmail.com>
2010-08-20 12:00:34 +0300

    haml first try

--- config/environment.rb
+++ config/environment.rb
@@ -19,10 +19,11 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-   config.gem "formtastic"
-   config.gem "paperclip"
+  config.gem "formtastic"
+  config.gem "paperclip"
 	config.gem 'awesome_nested_set'
 	config.gem 'jrails'
+	config.gem 'haml'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
