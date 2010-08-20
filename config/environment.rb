e00fa97cd2078cdaa6cc134faf259c784e26842f
kubus-ruby <kubus.ruby@gmail.com>
2010-08-20 11:36:24 +0300

    jQuery

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,8 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
    config.gem "formtastic"
    config.gem "paperclip"
-	 config.gem 'awesome_nested_set'
+	config.gem 'awesome_nested_set'
+	config.gem 'jrails'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
