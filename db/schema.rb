4d7da011c5835e7bb332f6f4f4d80617b4bbd600
kubus <kubus.ruby@gmail.com>
2010-08-31 14:57:18 +0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -45,22 +45,12 @@ ActiveRecord::Schema.define(:version => 20100817090711) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "price",              :limit => 10, :default => 0
+    t.decimal  "price",              :precision => 8, :scale => 2, :default => 0.0
     t.string   "photo_file_name"
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
   end
-
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "name"
